93e282cc691ae2efa7006aea813990aadac10401
im-rahman1 <mjc8808@gmail.com>
2021-06-16 12:12:04 +0500

    Finalize Feature Block

---
+++ script/app.js
@@ -0,0 +1,35 @@
+// Collapsibles
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// Testimonial SlideShow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if(n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if(n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "inline-flex";
+}
